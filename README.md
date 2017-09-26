c8984a3c38cca9b4cf6ca1e07c08c5939026fd33
Matheus Fidelis <msfidelis@outlook.com>
2017-09-26 19:36:58 -0300

    AES

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # take-my-hash
 Crypto.js helper - for lazy people (like me) 
+
+## Quickstart
+
+```bash
+npm install --save take-my-hash
+```
+
+```javascript
+const tmh = require('take-my-hash');
+```
+
+## AES 
+
+### Encrypt
+
+```javascript
+
+let hash = tmh.AESEncrypt('my text', 'my key 123');
+let rawText = tmh.AESDecrypt(hash, 'my key 123');
+
+console.log(hash, rawText);
+
+```
+
--- test/crypto.test.js
+++ test/crypto.test.js
@@ -1,1 +1,30 @@
-'use strict';
+"use strict";
+
+const tmh = require("../lib/take-my-hash");
+
+const Lab = require("lab");
+const lab = (exports.lab = Lab.script());
+
+const suite = lab.suite;
+const test = lab.test;
+
+const expect = require("chai").expect;
+const assert = require("chai").assert;
+
+const mockTxt = "yabadabadoo";
+const mockKey = "123123";
+
+
+suite("#Crypto Test", () => {
+  test("AES Encrypt/Decrypt", done => {
+
+    let AESHash = tmh.AESEncrypt(mockTxt, mockKey);
+    expect(AESHash).to.be.a('string');
+
+    let AESRaw = tmh.AESDecrypt(AESHash, mockKey);
+    expect(AESRaw).to.be.a('string');
+    expect(AESRaw).to.be.equal(mockTxt);
+
+    done();
+  });
+});
